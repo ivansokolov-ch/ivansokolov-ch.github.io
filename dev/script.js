1e32ec83274167f5ae50bd0a71619d74e31b5eb5
GitHub Action <action@github.com>
2022-09-30 17:16:31 +0000

    deploy files

--- dev/script.js
+++ dev/script.js
@@ -12,26 +12,63 @@ const img4 = document.querySelector("#img4");
 const img5 = document.querySelector("#img5");
 const img6 = document.querySelector("#img6");
 
+const text_standard = document.querySelector("#first");
+const text_second_standard = document.querySelector("#second");
+const text_third_standard = document.querySelector("#third");
+
+console.log(text_second_standard);
+
 radio1.addEventListener("click", () => {
-  img_standard.src = img1.src;
+  // img_standard.src = img1.src;
+  text_standard.innerHTML = "Fabri Fibra";
+  text_second_standard.innerHTML = "Caos Tour - 2022";
+  text_third_standard.innerHTML = "Fotografia";
 });
 
 radio2.addEventListener("click", () => {
-  img_standard.src = img2.src;
+  // img_standard.src = img2.src;
+  text_standard.innerHTML = "Drytech";
+  text_second_standard.innerHTML = "ADV";
+  text_third_standard.innerHTML = "Regia - Montaggio";
 });
 
 radio3.addEventListener("click", () => {
-  img_standard.src = img3.src;
+  // img_standard.src = img3.src;
+  text_standard.innerHTML = "Fashion";
+  text_second_standard.innerHTML = "";
+  text_third_standard.innerHTML = "Fotografia";
 });
 
 radio4.addEventListener("click", () => {
-  img_standard.src = img4.src;
+  // img_standard.src = img4.src;
+  text_standard.innerHTML = "Anagrafe";
+  text_second_standard.innerHTML = "";
+  text_third_standard.innerHTML = "Direzione Artistica";
 });
 
 radio5.addEventListener("click", () => {
-  img_standard.src = img5.src;
+  // img_standard.src = img5.src;
+  text_standard.innerHTML = "Moda";
+  text_second_standard.innerHTML = "";
+  text_third_standard.innerHTML = "Fotografia";
 });
 
 radio6.addEventListener("click", () => {
-  img_standard.src = img6.src;
+  // img_standard.src = img6.src;
+  text_standard.innerHTML = "Alpen Group";
+  text_second_standard.innerHTML = "";
+  text_third_standard.innerHTML = "Fotografia - Video";
 });
+
+Array.from(document.getElementsByClassName("radiobtn")).forEach((item) =>
+  item.addEventListener("click", (_) => {
+    img_standard.classList.remove("fade");
+    setTimeout(() => {
+      img_standard.classList.add("fade");
+      setTimeout(() => {
+        console.log(window.img);
+        img_standard.src = document.querySelector(`#img${item.id[5]}`).src;
+      }, 400);
+    }, 50);
+  })
+);
