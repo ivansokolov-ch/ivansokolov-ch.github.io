6133fbb684746791330bafce55fdd278d7bab121
giuliobosco <giuliobva@gmail.com>
2022-12-26 01:14:53 +0100

    integrate api projects

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const url = "http://localhost:8080";
+const apiUrl = "https://cms.ivansokolov.ch";
+
 const radio1 = document.querySelector("#radio1");
 const radio2 = document.querySelector("#radio2");
 const radio3 = document.querySelector("#radio3");
@@ -17,42 +20,87 @@ const text_standard = document.querySelector("#first");
 const text_second_standard = document.querySelector("#second");
 const text_third_standard = document.querySelector("#third");
 
-console.log(text_second_standard);
+const projectToRadio = (i) => {
+  return `<input ${i === 0?"checked":""} type="radio" name="radio_btn" class="radiobtn" id="radio${i}"/>`;
+}
 
-radio1.addEventListener("click", () => {
-  text_standard.innerHTML = "Fabri Fibra";
-  text_second_standard.innerHTML = "Caos Tour - 2022";
-  text_third_standard.innerHTML = "Fotografia";
-});
+const strapiUrlToExternalUrl = (url) => {
+  return `${apiUrl}${url}`;
+}
 
-radio2.addEventListener("click", () => {
-  text_standard.innerHTML = "Drytech";
-  text_second_standard.innerHTML = "ADV";
-  text_third_standard.innerHTML = "Regia - Montaggio";
-});
+const fillArticle = (e,i) => {
+  const article = document.createElement("article");
+  if (i % 2 === 0) {
+    article.innerHTML = `<div class="container_img">
+            <a href="${url}/project?id=${e.id}" title="${e.attributes.Title}">
+                <img alt="${e.attributes.Title}" src="${strapiUrlToExternalUrl(e.attributes.cover.data.attributes.formats.large.url)}"/>
+            </a>
+        </div>
+        <div class="container_text">
+            <div class="container_info">
+                <h2 class="primary_text">${e.attributes.Title}</h2>
+                <hr>
+                <p style="align-self: flex-end;" class="primary_color">${e.attributes.subtitle}</p>
+            </div>
+        </div>`;
+  } else {
+    article.style.justifyContent = "flex-end";
+    article.innerHTML = `<div class="container_text" style="justify-content: flex-start;">
+            <div class="container_info">
+                <h2 class="primary_text">
+                    ${e.attributes.Title}
+                </h2>
+                <hr>
+                <p style="align-self: flex-end;" class="primary_color">${e.attributes.subtitle}</p>
+            </div>
+        </div>
+        <div class="container_img" style="width: 49.8%;">
+            <a href="${url}/project?id=${e.id}" title="${e.attributes.Title}">
+                <img alt="${e.attributes.Title}" src="${strapiUrlToExternalUrl(e.attributes.cover.data.attributes.formats.large.url)}" />
+            </a>
+        </div>`;
+  }
 
-radio3.addEventListener("click", () => {
-  text_standard.innerHTML = "Fashion";
-  text_second_standard.innerHTML = "";
-  text_third_standard.innerHTML = "Fotografia";
-});
+  return article;
+}
 
-radio4.addEventListener("click", () => {
-  text_standard.innerHTML = "Anagrafe";
-  text_second_standard.innerHTML = "";
-  text_third_standard.innerHTML = "Direzione Artistica";
-});
+const projects = new XMLHttpRequest();
+projects.overrideMimeType("application/json");
+projects.open("GET", `${apiUrl}/api/projects?populate[0]=cover&sort[0]=id`);
+projects.onreadystatechange = () => {
+  if (projects.readyState === 4 && projects.status === 200) {
+    const data = JSON.parse(projects.responseText).data;
 
-radio5.addEventListener("click", () => {
-  text_standard.innerHTML = "Moda";
-  text_second_standard.innerHTML = "";
-  text_third_standard.innerHTML = "Fotografia";
-});
+    for (let i = 0; i < data.length; i++) {
+      document.querySelector("main .slides").innerHTML += projectToRadio(i);
+    }
+    let elem = document.querySelector("main");
+    for (let i = 0; i < data.length; i++) {
+      document.querySelector(`#radio${i}`).addEventListener("click", () => {
+        text_standard.innerHTML = data[i].attributes.Title;
+        text_third_standard.innerHTML = data[i].attributes.subtitle;
+        a_standard.href = `${url}/project?id=${data[i].id}`;
+        img_standard.classList.remove("fade");
+        setTimeout(() => {
+          img_standard.classList.add("fade");
+          setTimeout(() => {
+            img_standard.src = strapiUrlToExternalUrl(data[i].attributes.cover.data.attributes.formats.large.url);
+          }, 400);
+        }, 50);
+      })
+      const e = fillArticle(data[i], i);
 
-radio6.addEventListener("click", () => {
-  text_standard.innerHTML = "Alpen Group";
-  text_second_standard.innerHTML = "";
-  text_third_standard.innerHTML = "Fotografia - Video";
+      elem.parentElement.insertBefore(e, elem.nextSibling);
+      elem = e;
+    }
+    if (data.length > 0) {
+      document.querySelector("#radio0").click();
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  projects.send();
 });
 
 Array.from(document.getElementsByClassName("radiobtn")).forEach((item) =>
